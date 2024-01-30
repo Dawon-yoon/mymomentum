c1187090c97170ca14051e49a5a4b0cf5d9ecf69
wonyhayashi <154343298+Dawon-yoon@users.noreply.github.com>
2024-01-30 20:10:06 +0900

    #2 update clock.js

--- js/clock.js
+++ js/clock.js
@@ -2,10 +2,11 @@ const clock = document.querySelector("#clock");
 
 function getClock() {
   const date = new Date();
-  const getHour = date.getHours();
-  const ampm = getHour >= 12 ? "PM" : "AM"; //조건문 참:거짓
-  clock.innerText = `${ampm} ${getHour}:${date.getMinutes()}`;
+  const getHour = String(date.getHours()).padStart(2, "0");
+  const ampm = parseInt(getHour) >= 12 ? "PM" : "AM"; //조건문 true:false
+  const getMiute = String(date.getMinutes()).padStart(2, "0");
+  clock.innerText = `${ampm}${getHour}:${getMiute}`;
 }
-
+//toLocaleTimeString();->이건 초까지 나와서 안씀.
 getClock();
 setInterval(getClock, 1000);
